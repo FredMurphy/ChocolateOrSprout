3cae0a7172304fb22f52ab2a46419d1965229df0
Fred Murphy <fred27murphy@gmail.com>
2023-12-11 21:02:15 +0000

    Progress on UI

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 #from guizero import App, Text, PushButton
+from player import Player
+from gamemode import GameMode
 from tkinter import *
 from tkinter import ttk
-#from buildhat import Hat, Motor, ColorDistanceSensor
+from datetime import datetime
+from buildhat import Hat
 #import time
 
-#hat = Hat()
-#print(hat.get())
+hat = Hat()
+print(hat.get())
 
-#app = App(title="Chocolate or Sprouts", width=1920, height=1080, bg='#ff0000')
-#welcome_message = Text(app, text="Welcome to Chocolate or Sprouts", size=40, font="Tahoma", color="white")
-#app.display()
-#app.set_full_screen()
+def set_question(question_text):
+    instructionLabel.config(text = question_text)
+    
+def doPolling():
+    global game_mode, red_player_ready, blue_player_ready
+    if (game_mode == GameMode.WaitingToStart):
+        if red_player_ready == False:
+            red_player_ready = (red_player.get_color() != "")
+            if red_player_ready:
+                set_question("Red player ready!")
+                
+        if blue_player_ready == False:
+            blue_player_ready = (blue_player.get_color() != "")
+            if blue_player_ready:
+                set_question("Blue player ready!")
+            
+        if (red_player_ready and blue_player_ready):
+            game_mode = GameMode.Playing
+            instructionLabel.config(text = "Let's play!")
+    
+    frame.after(1000, doPolling)
+            
+global game_mode
+game_mode = GameMode.WaitingToStart
+#print(game_mode)
+
+red_player = Player('D', 'A', 1)
+blue_player = Player('C', 'B', -1)
+red_player_ready = False
+blue_player_ready = False
+
 root = Tk()
 root.title("Chocolate or Sprouts")
 root.geometry("1920x1080")
@@ -27,11 +57,14 @@ style.configure("TFrame", background="#21138a")
 style.configure("Question.TLabel", font="Tahoma 20", background="#21138a", foreground="#ffffff")
 
 frame = ttk.Frame(root, borderwidth=2, padding=10)
-instructionLabel = ttk.Label(frame, text="Hold any card over the left hand scanner to get started", style="Question.TLabel").grid(column=0, row=0, columnspan=2)
+instructionLabel = ttk.Label(frame, text="Hold any card over the left hand scanner to get started", style="Question.TLabel")
+instructionLabel.grid(column=0, row=0, columnspan=2)
 label1 = ttk.Label(frame, text="1").grid(column=0, row=1)
 label3 = ttk.Label(frame, text="3").grid(column=1, row=1)
 
 
 frame.place(x=1900, y=400, width=1200, height=300, anchor="ne")
-
+frame.after(1000, doPolling)
 root.mainloop()
+
+
---
+++ player.py
@@ -0,0 +1,45 @@
+from buildhat import Motor, ColorDistanceSensor
+
+class Player:
+    
+    def __init__(self, sensor_port, motor_port, chocolate_direction):
+        self.sensor = ColorDistanceSensor(sensor_port)
+        self.motor = Motor(motor_port)
+        self.chocolate_direction = chocolate_direction
+        
+    def get_color(self):
+        distance = self.sensor.get_distance()
+        if (distance > 1):
+            # too far away
+            return ""
+        color = self.sensor.get_color_rgb()
+        min_val = min(color)
+        max_val = max(color)
+        # print('Color',color, min_val, max_val)
+
+        if (max_val < 30 or (max_val-min_val)< 5):
+            # not enough contrast
+            return ""
+
+        lower = (min_val+min_val+max_val)/3
+        upper = (min_val+max_val+max_val)/3
+        red = color[0]
+        green = color[1]
+        blue = color[2]
+
+        if (red > lower and red < upper) or (green > lower and green < upper) or (blue > lower and blue < upper):
+            # unclear
+            return ""
+
+        if (red > upper and green < lower and blue < lower):
+            return "red"
+        if (red < lower and green > upper and blue < lower):
+            return "green"
+        # more like cyan, but that's what we're using and we'll call it blue
+        if (red < lower and green > upper and blue > upper):
+            return "blue"
+        if (red > upper and green > upper and blue < lower):
+            return "yellow"
+
+        # some other colour
+        return ""
